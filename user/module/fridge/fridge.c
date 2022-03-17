44e2a7c601809d2b6b356280aea806e62f56551d
You Zhou <yz3883@columbia.edu>
2022-03-17 01:38:28 -0400

    partly part1

--- user/module/fridge/fridge.c
+++ user/module/fridge/fridge.c
@@ -7,6 +7,8 @@
 
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 #include "fridge_data_structures.h"
 
 #define MODULE_NAME "Fridge"
@@ -18,26 +20,101 @@ extern void * kkv_get_ptr;
 
 long kkv_init(int flags)
 {
-	static struct kkv_ht_bucket *hashtable = kmalloc(17*sizeof(kkv_ht_bucket));
+	static struct kkv_ht_bucket *hashtable = kmalloc(17*sizeof(struct kkv_ht_bucket), GFP_KERNEL);
+	int i = 0;
 	struct kkv_ht_bucket *current;
-	for (i=0; i<17, i++){
+
+	if (hashtable == NULL) {
+		printk(KERN_ERR "kmalloc() failed to allocate memory\n");
+		return -1;
+	}
+
+	for (i=0; i<17; i++){
 		current = hashtable+i;
 		spin_lock_init(current->lock);
-		INIT_LIST_HEAD(current->entries);
+		INIT_LIST_HEAD(&current->entries);
 		current->count = 0;
 	}
+	return 0;
 }
 
 long kkv_destory(int flags)
 {
-}
+	static struct kkv_ht_bucket *hashtable;
+	struct kkv_ht_bucket *current;
+	struct kkv_ht_entry *cur;
+	struct kkv_ht_entry *nxt;
+	struct list_head *tem_list;
+	long sum = 0;
 
-long kkv_put(uint32_t key, void __user *val, size_t size, int flags)
-{
+	for (i=0; i<17; i++){
+		current = hashtable + i;
+		tem_list = &current->entries;
+		list_for_each_entry_safe(cur, nxt, tem_list, entries){
+			list_del(&cur->entries);
+			kfree(cur);
+			current->count--;
+			sum++;
+		}
+	}
+	kfree(hashtable);
+	return sum;
 }
 
 long kkv_get(uint32_t key, void __user *val, size_t size, int flags)
 {
+	static struct kkv_ht_bucket *hashtable;
+	struct kkv_ht_bucket *current;
+	struct kkv_ht_entry *cur;
+	int index = key % 17;
+
+	current = hashtable + index;
+	list_for_each_entry(cur, &current->entries, entries){
+		if((cur->kv_pair).key == key){
+			list_del(&cur->entries);
+			current->count--;
+			if(copy_to_user(val, (cur->kv_pair).val, size))
+				return -EFAULT;
+			kfree(cur);
+			return 0;
+		}
+	}
+	return -ENOENT;
+}
+
+long kkv_put(uint32_t key, void __user *val, size_t size, int flags)
+{
+	static struct kkv_ht_bucket *hashtable;
+	struct kkv_ht_bucket *current;
+	struct kkv_ht_entry *cur;
+	struct kkv_ht_entry *new_entry;
+	int index = key % 17;
+
+	current = hashtable + index;
+	list_for_each_entry(cur, &current->entries, entries){
+		if ((cur->kv_pair).key == key){
+			if(copy_from_user((cur->kv_pair).val, val, size))
+				return -EFAULT;
+			return 0;
+		}
+	}
+	
+
+	new_entry = kmalloc(sizeof(struct kkv_ht_entry), GFP_KERNEL);
+	if (new_entry == NULL) {
+		printk(KERN_ERR "kmalloc() failed to allocate memory\n");
+		return -1;
+	}
+	INIT_LIST_HEAD(&new_entry->entries);
+	list_add_tail(&new_entry->entries, &current->entries);
+	(new_entry->kv_pair).key = key;
+	(new_entry->kv_pair).size = size;
+
+	//maybe we don't need to copy from use twice
+	if (copy_from_user((new_entry->kv_pair).val, val, size))
+		return -EFAULT;
+	current->count++;
+	return 0;
 }
 
 int fridge_init(void)
